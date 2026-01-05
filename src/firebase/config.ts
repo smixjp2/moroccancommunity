eeadfe4332b391b6e43830c0971c5b411bca1063
Mohammed Serrou <serroumohammed7@gmail.com>
2026-01-05 21:32:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "masi-insights-backup-366-74d81",
+  "appId": "1:942112569352:web:16ea61fbba6519cc02b9bd",
+  "apiKey": "AIzaSyCK1u5U4SvGEyTX_MlB7Ml5XTu9soha4Pc",
+  "authDomain": "masi-insights-backup-366-74d81.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "942112569352"
+};
