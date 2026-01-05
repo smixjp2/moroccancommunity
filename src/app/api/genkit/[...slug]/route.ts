36b98b44bb919a0f3513160ea9f75b43b0f18deb
Mohammed Serrou <serroumohammed7@gmail.com>
2026-01-05 19:45:09 +0000

    ca toujours mache pas et tu a desja fixé ce probléme auparavant trouve u

--- src/app/api/genkit/[...slug]/route.ts
+++
@@ -1,8 +0,0 @@
-'use server';
-
-import {ai} from '@/ai/genkit';
-import {genkitNextApiHandler} from '@genkit-ai/next';
-
-export const {GET, POST} = genkitNextApiHandler({
-  getGenkit: () => ai,
-});
