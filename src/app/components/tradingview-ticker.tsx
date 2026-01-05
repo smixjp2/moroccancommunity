e0e36cf1049427773eaa1074e2489281f1ea5cf6
Mohammed Serrou <serroumohammed7@gmail.com>
2026-01-05 22:34:01 +0000

    voi juste le preview que tu donne

--- src/app/components/tradingview-ticker.tsx
+++ src/app/components/tradingview-ticker.tsx
@@ -1,60 +1,33 @@
 // TradingViewWidget.tsx
 'use client';
 
-import React, { useEffect, useRef, memo } from 'react';
+import React, { memo } from 'react';
 
 function TradingViewTicker() {
-  const container = useRef<HTMLDivElement>(null);
-
-  useEffect(
-    () => {
-      if (container.current && container.current.children.length === 0) {
-        const script = document.createElement("script");
-        script.src = "https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js";
-        script.type = "text/javascript";
-        script.async = true;
-        script.innerHTML = `
-          {
+  return (
+    <div className="tradingview-widget-container">
+      <div className="tradingview-widget-container__widget"></div>
+      <script
+        type="text/javascript"
+        src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js"
+        async
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
             "symbols": [
-              {
-                "description": "MASI",
-                "proName": "CSE:MASI"
-              },
-              {
-                "description": "Attijariwafa Bank",
-                "proName": "CSE:ATW"
-              },
-              {
-                "description": "BCP",
-                "proName": "CSE:BCP"
-              },
-              {
-                "description": "Maroc Telecom",
-                "proName": "CSE:IAM"
-              },
-              {
-                "description": "LafargeHolcim",
-                "proName": "CSE:LHM"
-              },
-              {
-                "description": "Cosumar",
-                "proName": "CSE:CSR"
-              }
+              { "description": "MASI", "proName": "CSE:MASI" },
+              { "description": "Attijariwafa Bank", "proName": "CSE:ATW" },
+              { "description": "BCP", "proName": "CSE:BCP" },
+              { "description": "Maroc Telecom", "proName": "CSE:IAM" },
+              { "description": "LafargeHolcim", "proName": "CSE:LHM" },
+              { "description": "Cosumar", "proName": "CSE:CSR" }
             ],
             "showSymbolLogo": true,
-            "isTransparent": false,
+            "isTransparent": true,
             "displayMode": "adaptive",
             "locale": "fr"
-          }`;
-        container.current.appendChild(script);
-      }
-    },
-    []
-  );
-
-  return (
-    <div className="tradingview-widget-container" ref={container}>
-      <div className="tradingview-widget-container__widget"></div>
+          })
+        }}
+      />
     </div>
   );
 }
