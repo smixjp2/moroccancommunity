7ca1be31281ad4d1f47929b003dbc4c6285e199c
Mohammed Serrou <serroumohammed7@gmail.com>
2026-01-07 20:55:10 +0000

    File changes

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    BREVO_API_KEY: process.env.BREVO_API_KEY,
+    BREVO_LIST_ID: process.env.BREVO_LIST_ID,
+  }
 };
 
 export default nextConfig;
