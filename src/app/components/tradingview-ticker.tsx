c691a0bc28734db1782b759502edab837c48deb4
Mohammed Serrou <serroumohammed7@gmail.com>
2026-01-08 15:53:05 +0000

    je ne vois toujours pas la bare des actions et l'evolution du masi

--- src/app/components/tradingview-ticker.tsx
+++ src/app/components/tradingview-ticker.tsx
@@ -1,33 +1,38 @@
 // TradingViewWidget.tsx
 'use client';
 
-import React, { memo } from 'react';
+import React, { useEffect, useRef, memo } from 'react';
 
 function TradingViewTicker() {
+  const container = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (container.current && container.current.children.length === 0) {
+      const script = document.createElement('script');
+      script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js';
+      script.type = 'text/javascript';
+      script.async = true;
+      script.innerHTML = JSON.stringify({
+        "symbols": [
+          { "description": "MASI", "proName": "CSE:MASI" },
+          { "description": "Attijariwafa Bank", "proName": "CSE:ATW" },
+          { "description": "BCP", "proName": "CSE:BCP" },
+          { "description": "Maroc Telecom", "proName": "CSE:IAM" },
+          { "description": "LafargeHolcim", "proName": "CSE:LHM" },
+          { "description": "Cosumar", "proName": "CSE:CSR" }
+        ],
+        "showSymbolLogo": true,
+        "isTransparent": false,
+        "displayMode": "adaptive",
+        "locale": "fr"
+      });
+      container.current.appendChild(script);
+    }
+  }, []);
+
   return (
-    <div className="tradingview-widget-container">
-      <div className="tradingview-widget-container__widget"></div>
-      <script
-        type="text/javascript"
-        src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js"
-        async
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            "symbols": [
-              { "description": "MASI", "proName": "CSE:MASI" },
-              { "description": "Attijariwafa Bank", "proName": "CSE:ATW" },
-              { "description": "BCP", "proName": "CSE:BCP" },
-              { "description": "Maroc Telecom", "proName": "CSE:IAM" },
-              { "description": "LafargeHolcim", "proName": "CSE:LHM" },
-              { "description": "Cosumar", "proName": "CSE:CSR" }
-            ],
-            "showSymbolLogo": true,
-            "isTransparent": true,
-            "displayMode": "adaptive",
-            "locale": "fr"
-          })
-        }}
-      />
+    <div className="tradingview-widget-container" ref={container} style={{ height: "46px", width: "100%" }}>
+      <div className="tradingview-widget-container__widget" style={{ height: "calc(100% - 32px)", width: "100%" }}></div>
     </div>
   );
 }
