487b0e2637baa96718f866812a461800639a845f
Mohammed Serrou <serroumohammed7@gmail.com>
2026-01-12 20:19:30 +0000

    `DialogContent` requires a `DialogTitle` for the component to be accessi

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -64,10 +64,16 @@ const SheetContent = React.forwardRef<
       className={cn(sheetVariants({ side }), className)}
       {...props}
     >
+      <SheetPrimitive.Title asChild>
+        <h2 className="sr-only">Menu de Navigation</h2>
+      </SheetPrimitive.Title>
+      <SheetPrimitive.Description asChild>
+        <p className="sr-only">Sélectionnez une option pour naviguer vers une nouvelle page.</p>
+      </SheetPrimitive.Description>
       {children}
       <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
         <X className="h-4 w-4" />
-        <span className="sr-only">Close</span>
+        <span className="sr-only">Fermer</span>
       </SheetPrimitive.Close>
     </SheetPrimitive.Content>
   </SheetPortal>
